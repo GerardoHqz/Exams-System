ec07f9cb843ad9c8ead24f37ed57d018ed99f25a
GerardoHqz <gerardohenriquez.sv@gmail.com>
2024-02-06 17:53:42 -0600

    :tada: init project

---
+++ exams-system-backend/src/test/java/com/exams/system/examssystembackend/ExamsSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exams.system.examssystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
